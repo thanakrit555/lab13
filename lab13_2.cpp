b6904234aa42324a35620c012262bad29f637dca
thanakrit555 <59599161+thanakrit555@users.noreply.github.com>
2021-01-22 16:57:15 +0700

    Update lab13_2.cpp

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -3,11 +3,39 @@ using namespace std;
 
 const int N = 5;
 
-void inputMatrix(double [][N]);
+void inputMatrix(double A[][N]){
+	for(int i=0;i<N;i++){
+		cout <<"Row "<< i+1 <<": ";
+		for(int j=0;j<N;j++){
+		cin >> A[i][j];
 
-void findLocalMax(const double [][N], bool [][N]);
+	}
 
-void showMatrix(const bool [][N]);
+}
+}
+
+void findLocalMax(const double A[][N], bool B[][N]){
+for(int i=0;i<N;i++){
+		for(int j =0 ; j < N ; j++ ){
+			B[i][j] = 0;
+		}
+	}
+
+	for(int i=1 ; i<N -1 ; i++){
+		for(int j =1 ; j < N-1 ; j++ ){
+			if(A[i][j] >= A[i-1][j] && A[i][j] >= A[i+1][j] && A[i][j] >= A[i][j-1] && A[i][j] >= A[i][j+1]  ) B[i][j] = 1;
+			else B[i][j] = 0;
+		}
+	}
+}
+
+void showMatrix(const bool B[][N]){
+	for(int i =0;i<N;i++){
+		for(int j =0;j<N;j++) cout<<B[i][j]<<" ";
+		cout <<"\n";
+	}
+
+}
 
 int main(){
 	double A[N][N]; 
